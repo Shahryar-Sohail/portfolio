16350f70d5d33a768996b9fd1221bd58545fdc53
Shahryar-Sohail <shahryar.ch123@gmail.com>
2025-05-24 16:44:53 +0500

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/portfolio/', // 👈 This is required for GitHub Pages
   plugins: [react()],
 })
